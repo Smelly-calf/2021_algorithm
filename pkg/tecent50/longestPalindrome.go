f3c9f130523a6654fe0966ef68c38a5864aca243
王倩 <wangqian445@jd.com>
2021-11-03 15:13:19 +0800

    add

--- pkg/tecent50/longestPalindrome.go
+++ pkg/tecent50/longestPalindrome.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 // 最长回文字串
-// 解法1：用动态规划寻找两个字符串最长公共子串的方法，寻找最长回文子串，边界条件是 maxI=Len-1-maxJ+arr[maxI][maxJ]-1，maxI表示最长子串末尾下标
+// 解法1：用动态规划寻找两个字符串最长公共子串的方法，还需要判断该字符串倒置后下标和倒置前是否匹配，只用比较末尾下标maxJ和倒置前是否一样就好。
+// 已知maxI表示最长子串的末尾下标，maxJ倒置前下标加最长子串长度应该也是子串的末尾，应该等于maxI，即满足 maxI = Len-1-maxJ+arr[maxI][maxJ]-1 就说明是回文串。
+// 至于为什么只比较末尾，先这么理解吧：如果不是回文串，末尾下标一定不匹配。
+// case: caba，abc435cba，babad，cbbd，a，ac
 func longestPalindrome(s string) string {
 	if s == "" {
 		return ""
@@ -18,11 +21,11 @@ func longestPalindrome(s string) string {
 		d[i] = make([]int, lens)
 	}
 	// d[i][j]保存00到ij的最大公共子串长度
-	var maxLen int // maxLen保存最大公共子串长度
-	var maxEnd int // maxEnd保存最大公共子串的末尾下标
+	var maxLen int // maxLen 保存最大公共子串长度
+	var maxEnd int // maxEnd 保存最大公共子串的末尾下标
 	for i := 0; i < lens; i++ {
 		for j := 0; j < lens; j++ {
-			if s[i] == reverse[j] {
+			if s[i] == reverse[j] { // 相等的时候才加1
 				if i == 0 || j == 0 {
 					d[i][j] = 1
 				} else {
@@ -30,16 +33,14 @@ func longestPalindrome(s string) string {
 				}
 			}
 			if d[i][j] > maxLen {
-				// 保存maxLen时判断边界条件
+				// 保存maxLen时判断边界条件: Len-1-maxJ+arr[maxI][maxJ]-1=maxI, sf=Len-1-maxJ 表示maxJ反转前的下标，sf+arr[maxI][maxJ]-1表示maxJ反转前下标加上最长公共子串的长度对应的下标，
+				// 等于maxI则表示最长公共子串就是要找的回文子串.
 				sf := lens - 1 - j //sf意思为reverse下标j对应的s的下标
 				if sf+d[i][j]-1 == i {
 					maxLen = d[i][j]
 					maxEnd = i
-					fmt.Println(maxEnd)
-					fmt.Println(maxLen)
 				}
 			}
-
 		}
 	}
 	return s[maxEnd-maxLen+1 : maxEnd+1]
@@ -56,6 +57,6 @@ func reverseS(s string) string {
 }
 
 func main() {
-	s := "aacdefcaa"
+	s := "caba"
 	fmt.Println("最长回文子串:", longestPalindrome(s))
 }
