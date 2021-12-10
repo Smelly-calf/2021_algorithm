a373f5bdfae98a9e9bdf856f73dbf9a4e7a70bf7
王倩 <wangqian445@jd.com>
2021-12-10 20:37:10 +0800

    add links and nums

---
+++ pkg/string/multiply.go
@@ -0,0 +1,7 @@
+package main
+
+// 字符串相乘
+// 方法一：利用nums1第i位和nums2第j位的乘积的下标规律把每一位乘积的结果存储在数组中，结果数组的长度为m+n，nums1[i]*nums2[j]的结果位于arr[i+j+1]，进位位于arr[i+j]。
+func multiply(nums1 string, nums2 string){
+	return
+}
