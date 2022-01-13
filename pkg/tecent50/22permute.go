698a43b1f727711a36d3195f21f01ab6c6df1692
王倩 <wangqian445@jd.com>
2022-01-13 20:28:05 +0800

    回溯算法解全排列

---
+++ pkg/tecent50/22permute.go
@@ -0,0 +1,11 @@
+package main
+
+// 全排列
+// nums = [1,2,3]
+// 思路：回溯算法，深度优先遍历的思想
+// path保存每次遍历的栈，当path的深度depth等于数组长度，表示一次搜索完成。状态数组used存储path元素状态，需要重置。
+// https://leetcode-cn.com/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/
+func permute(nums []int) [][]int {
+	res := make([][]int, 0)
+	return res
+}
